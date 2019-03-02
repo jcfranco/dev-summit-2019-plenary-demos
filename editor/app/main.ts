fa54de89b7c81fc7b07186c4b818d7e2c8555ce9
JC Franco <jfranco@esri.com>
2019-03-02 10:06:45 -0800

    Use hiking trail service for Editor sample.

--- editor/app/main.ts
+++ editor/app/main.ts
@@ -2,12 +2,10 @@ import WebMap = require("esri/WebMap");
 import MapView = require("esri/views/MapView");
 import Editor = require("esri/widgets/Editor");
 
-import GroupByFunction = __esri.GroupByFunction;
-
 // Create a map from the referenced web map item id
 const webmap = new WebMap({
   portalItem: {
-    id: "2e2ccb2c1cb14cf69631547e0c724bd7"
+    id: "e10fb3fab173489f83382624a81538aa"
   }
 });
 
@@ -19,42 +17,7 @@ const view = new MapView({
   }
 });
 
-// Create a custom group to separate the different areas of crime
-// This function takes an object containing a feature layer and feature template
-const groupBySeverity: GroupByFunction = ({ template }) => {
-  let groupHeading: string;
-
-  switch (template.name) {
-    case "Criminal Homicide":
-    case "Rape":
-    case "Robbery":
-    case "Aggravated Assault":
-      groupHeading = "Violent Crime";
-      break;
-
-    case "Arson":
-    case "Burglary":
-    case "Larceny":
-    case "Motor Vehicle Theft":
-      groupHeading = "Property Crime";
-      break;
-
-    default:
-      groupHeading = "Quality of Life";
-  }
-
-  return groupHeading;
-};
-
 // Create the Editor
-const editor = new Editor({
-  view: view,
-  // Override the default template behavior of the Editor widget
-  supportingWidgetDefaults: {
-    featureTemplates: {
-      groupBy: groupBySeverity
-    }
-  }
-});
+const editor = new Editor({ view });
 
 view.ui.add(editor, "top-right");
